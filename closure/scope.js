20e1a77c5f49ab4295d5b92696f59da683e2f040
Apurbalal Singha <apurbalal@users.noreply.github.com>
2024-02-09 19:15:10 +0900

    Add files via upload

---
+++ closure/scope.js
@@ -0,0 +1,17 @@
+// console.log("Var output")
+// for (var i = 0; i < 10; i++) {
+//   // using setTimeout so that it will be executed after the loop
+//   setTimeout(() => console.log(i), 0);
+// }
+
+console.log("\nLet output")
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => console.log(i), 0);
+}
+
+console.log("\nLet output")
+for (var i = 0; i < 10; i++) {
+  (function (value) {
+    setTimeout(() => console.log(value), 0);
+  })(i)
+}
