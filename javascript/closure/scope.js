6a754d3bac021102c4b78ee4d34eea26a6129c0a
Apurbalal Singha <apurbalal@users.noreply.github.com>
2024-02-10 14:24:27 +0900

    Update scope.js

--- javascript/closure/scope.js
+++ javascript/closure/scope.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 10; i++) {
   setTimeout(() => console.log(i), 0);
 }
 
-console.log("\nLet output")
+console.log("\nFixed Var output")
 for (var i = 0; i < 10; i++) {
   (function (value) {
     setTimeout(() => console.log(value), 0);
