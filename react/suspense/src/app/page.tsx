ff63c4b208e8b187c46f022cbea1a2f114d56b4c
Apurbalal Singha <apurbalal@users.noreply.github.com>
2024-02-23 23:49:28 +0900

    Fix

--- react/suspense/src/app/page.tsx
+++ react/suspense/src/app/page.tsx
@@ -1,11 +1,44 @@
 "use client";
 import { cache, suspendedFetch } from "@/utils/suspendedFetch";
 import styles from "./page.module.css";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
-const CompA = () => {
+const SuspendedComp = () => {
   const result = suspendedFetch(new URL("https://jsonplaceholder.typicode.com/posts/1"));
-  return <div>{JSON.stringify(result)}</div>;
+  return (
+    <div style={{ border: "1px solid white", borderRadius: 8, padding: 12 }}>
+      <h2>With suspense</h2>
+      {JSON.stringify(result)}
+    </div>
+  );
+}
+
+const NormalComp = () => {
+  const [result, setResult] = useState<unknown>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const init = async () => {
+      setLoading(true);
+      const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1").then(resp => resp.json());
+      setResult(resp);
+      setLoading(false);
+    }
+    void init();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  return (
+    <div style={{ border: "1px solid white", borderRadius: 8, padding: 12 }}>
+      <h2>Without suspense</h2>
+      {JSON.stringify(result)}
+    </div>
+  )
 }
 
 export default function Home() {
@@ -13,10 +46,11 @@ export default function Home() {
   cache.clear();
   return (
     <main className={styles.main}>
-      <h2>Suspense</h2>
+      <h2>Suspense Data Fetching</h2>
       <Suspense fallback={<div>Loading...</div>}>
-        <CompA />
+        <SuspendedComp />
       </Suspense>
+      <NormalComp />
     </main>
   );
 }
