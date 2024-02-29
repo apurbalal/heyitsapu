e2523e6dfc6d6bd51bf3f635161a08241ca4c43d
Apurbalal Singha <apurbalal@users.noreply.github.com>
2024-02-29 21:31:21 +0900

    Uncommited changes

--- react/use-sync-external-store/src/App.js
+++ react/use-sync-external-store/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { useLocalStorage } from './hook/useLocalStorage';
-// import { useLocalSyncStorage } from './hook/useLocalSyncStorage';
+import { useLocalSyncStorage } from './hook/useLocalSyncStorage';
 
 const generateRandom = () => Math.random().toString(36).substring(7);
 
 const Counter1 = () => {
-  const [random, setRandom] = useLocalStorage("random-1");
+  const [_random, setRandom] = useLocalSyncStorage("random-1");
   return (
-    <div>
-      <p>{JSON.stringify(random)}</p>
-      <button onClick={() => setRandom({ value: generateRandom() })}>Increment</button>
+    <div style={{ border: "1px solid white", borderRadius: "2px", textAlign: "center", padding: "24px" }}>
+      <p>Component A</p>
+      <p>Random: {_random.value}</p>
+      <button onClick={() => setRandom({ value: generateRandom() })}>Update local storage</button>
     </div>
   );
 }
